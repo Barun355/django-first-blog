87a0d64b1635be112575b695e3153b7bdb5d4a19
Barun355 <baruntiwary620@gmail.com>
2023-03-10 18:56:39 +0530

    updated the source code with personal navigator

--- mysite/views.py
+++ mysite/views.py
@@ -5,3 +5,11 @@ def index(request):
 
 def about(request):
     return HttpResponse('About Page')
+
+def navigator(request):
+    return HttpResponse('''
+        <h1>Personal Navigator</h1><br>
+        <span><a href="www.google.com" target="_blank">Goolge</a></span><br>
+        <span><a href="https://github.com/Barun355" target="_blank">Github</a></span><br>
+        <span><a href="https://stackoverflow.com/users/19755163/barun-tiwary-21me60" target="_blank">StackOverflow</a></span><br>
+    ''')
